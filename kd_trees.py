ac2bc692c8c2e5182c8da17d24608b0ea47f906f
Daniel Hono <dhono@albany.edu>
2014-05-14 11:54:06 -0400

    First commit.

---
+++ kd_trees.py
@@ -0,0 +1,65 @@
+#This is my implementation of K-d Trees as described in the original paper by Bently
+#Daniel S. Hono II - 2014/5/14 
+#Univeristy at Albany, SUNY 
+
+#A classwrapper for a kd-Tree. 
+#Contains root object, an instance of the kdNode class.  
+class kdTree:
+    def __init__(self, dim):
+        self.root = None; #Start the tree empty 
+        self.k = dim; #Set the dimension 
+
+    #Create a new kdNode instances and return it. 
+    def newNode(self, d, v):
+        tmp = kdNode(None, None, d);
+        tmp.kList = v;
+        return tmp;
+
+    #Insert a node in the tree using Bently's algorithm. 
+    def kdInsert(self, node, point, i):
+        if self.root is None: #Empty tree
+            self.root = self.newNode(i, point);
+            return;
+        elif node is None:
+            return self.newNode(i, point);
+        else:       
+            if node.getDiscrimKey() < point[i]:
+                i += 1 % self.k;
+                node.HISON = self.kdInsert(node.HISON, point, i);
+            else:
+                i+= 1 % self.k; 
+                node.LOSON = self.kdInsert(node.LOSON, point, i); 
+
+#A class the represents a kd-tree node. 
+#Each node contains a left and right child pointer,
+#keys (tuple), and a discriminator
+class kdNode:
+    #On creation of new kd-node it must have a LO and HI pointer.
+    #These pointers may be null. 
+    def __init__(self, LOSON, HISON, DISC):
+        self.LOSON = LOSON;
+        self.HISON = HISON;
+        self.DISCRIM = DISC; 
+        self.kList = None;
+
+    def getDiscrimKey(self):
+        return self.kList[self.DISCRIM];
+
+    def nextDisc(self, k):
+        return (self.DISCRIM + 1) % k; 
+
+            
+def main():
+    T = kdTree(); #Initialize a new kdTree instances. 
+    print T.root;
+    T.kdInsert(T.root, (5, 4, 1), 0);
+    print T.root.kList;
+    T.kdInsert(T.root, (1, 4, 2), 0);
+    print T.root.LOSON.kList;
+    T.kdInsert(T.root, (6, 7, 4), 0);
+    print T.root.HISON.kList;
+    T.kdInsert(T.root, (3, 6, 4), 0);
+    print T.root.LOSON.HISON;
+
+if __name__ == 'main':
+    main();
